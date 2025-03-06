4e26cc0a0f6c9b7d15a69a362ea3111d42eb0494
MigoXV <1695587905@qq.com>
2025-03-06 21:02:03 +0800

    准备构建训练程序

--- tests/raw_draw.py
+++ tests/raw_draw.py
@@ -12,6 +12,7 @@ from scipy.interpolate import griddata
 plt.rcParams["font.sans-serif"] = ["SimHei"]
 plt.rcParams["axes.unicode_minus"] = False
 
+
 def temp_correction(tempData):
     tempData = (
         -7.8836 * 10**-6 * tempData**3
@@ -22,63 +23,68 @@ def temp_correction(tempData):
     return tempData
 
 
+HEIGHT = 288
+WIDTH = 384
 
 
-x1 = []
-y1 = []
-z1 = []
-data = np.genfromtxt("data-bin/img_dep_384x288.csv", delimiter=",").astype(np.float32)
-dataPoint = np.ndarray(shape=(288 * 384, 3), dtype=float, order="F")
-for i in range(288):
-    for j in range(384):
-        x1.append(i + 1)
-        y1.append(j + 1)
-        z1.append(data[i + 1, j + 1])
-for i in range(288 * 384):
-    dataPoint[i, 0] = x1[i]
-    dataPoint[i, 1] = y1[i]
-    dataPoint[i, 2] = z1[i]
+def draw_depth():
+    data = np.genfromtxt("data-bin/img_dep_384x288.csv", delimiter=",").astype(
+        np.float32
+    )
+    x1 = np.repeat(np.arange(1, HEIGHT + 1), WIDTH)
+    y1 = np.tile(np.arange(1, WIDTH + 1), HEIGHT)
+    z1 = data[1:, 1:].flatten()
 
-x = dataPoint[:, 0]
-y = dataPoint[:, 1]
-z = dataPoint[:, 2]
-xi = np.linspace(min(x), max(x))
-yi = np.linspace(min(y), max(y))
-xi, yi = np.meshgrid(xi, yi)
-zi = griddata(dataPoint[:, 0:2], z, (xi, yi), method="cubic")
+    xi = np.linspace(1, HEIGHT)
+    yi = np.linspace(1, WIDTH)
+    xi, yi = np.meshgrid(xi, yi)
+    zi = griddata(np.vstack((x1, y1)).T, z1, (xi, yi), method="cubic")
 
-fig1 = plt.figure(1)
-ax = plt.axes(projection="3d")
-pickle.dump((xi, yi, zi), open("data-bin/raw_dep.pkl", "wb"))
-surf = ax.plot_surface(xi, yi, zi, cmap="BuPu", linewidth=0, antialiased=False)
-fig1.colorbar(surf)
-ax.set_title("深度图")
-plt.xlabel("x")
+    fig1 = plt.figure(1)
+    ax = plt.axes(projection="3d")
+    surf = ax.plot_surface(xi, yi, zi, cmap="BuPu", linewidth=0, antialiased=False)
+    fig1.colorbar(surf)
+    ax.set_title("深度图")
+    plt.xlabel("x")
+    plt.show()
 
 
-data = scio.loadmat("data-bin/tempData.mat")
-tempData = np.array(data["thermalImage"])  # 将matlab数据赋值给python变量
-dataPoint = np.ndarray(shape=(288 * 382, 3), dtype=float, order="F")
-print(tempData)
-for i in range(288):
-    for j in range(382):
-        tempData[i, j] = temp_correction(tempData[i, j])
-        dataPoint[i * 382 + j, 0] = i
-        dataPoint[i * 382 + j, 1] = j
-        dataPoint[i * 382 + j, 2] = tempData[i, j]
-x = dataPoint[:, 0]
-y = dataPoint[:, 1]
-z = dataPoint[:, 2]
-xi = np.linspace(min(x), max(x))
-yi = np.linspace(min(y), max(y))
-xi, yi = np.meshgrid(xi, yi)
-zi = griddata(dataPoint[:, 0:2], z, (xi, yi), method="cubic")
+def draw_temperature():
+    new_temp_correction = (
+        lambda x: -7.8836 * 10**-6 * x**3 + 0.0176 * x**2 - 10.815 * x + 2725.9628
+    )
+    data = scio.loadmat("data-bin/tempData.mat")
+    temp_data = np.array(data["thermalImage"])  # 将matlab数据赋值给python变量
+    # old_temp_data=temp_correction(temp_data)
+    new_temp_data = new_temp_correction(temp_data)
+    data_point = np.ndarray(shape=(288 * 382, 3), dtype=float, order="F")
+    print(temp_data)
+    temp_data2 = np.zeros_like(temp_data,dtype=float)
+    for i in range(288):
+        for j in range(382):
+            temp_data2[i, j] = temp_correction(temp_data[i, j])
+            data_point[i * 382 + j, 0] = i
+            data_point[i * 382 + j, 1] = j
+            data_point[i * 382 + j, 2] = temp_data2[i, j]
+    x1 = np.repeat(np.arange(1, 288 + 1), 382)
+    y1 = np.tile(np.arange(1, 382 + 1), 288)
+    x = data_point[:, 0]
+    y = data_point[:, 1]
+    z = data_point[:, 2]
+    xi = np.linspace(min(x), max(x))
+    yi = np.linspace(min(y), max(y))
+    xi, yi = np.meshgrid(xi, yi)
+    zi = griddata(data_point[:, 0:2], z, (xi, yi), method="cubic")
+
+    fig2 = plt.figure(2)
+    ax = plt.axes(projection="3d")
+    surf = ax.plot_surface(xi, yi, zi, cmap="BuPu", linewidth=0, antialiased=False)
+    pickle.dump((xi, yi, zi), open("data-bin/raw_temp.pkl", "wb"))
+    fig2.colorbar(surf)
+    ax.set_title("温度图")
+    plt.show()
 
 
-fig2 = plt.figure(2)
-ax = plt.axes(projection="3d")
-surf = ax.plot_surface(xi, yi, zi, cmap="BuPu", linewidth=0, antialiased=False)
-pickle.dump((xi, yi, zi), open("data-bin/raw_temp.pkl", "wb"))
-fig2.colorbar(surf)
-ax.set_title("温度图")
-plt.show()
+if __name__ == "__main__":
+    # draw_depth()
+    draw_temperature()
